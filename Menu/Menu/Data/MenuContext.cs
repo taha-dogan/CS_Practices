509232ca9fd9372ac77f06939a78e24bf7e04845
flakysob <flakysob@gmail.com>
2024-05-11 15:53:56 +0300

    First Commit

---
+++ Menu/Menu/Data/MenuContext.cs
@@ -0,0 +1,62 @@
+﻿using Menu.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Menu.Data
+{
+    public class MenuContext : DbContext
+    {
+        public MenuContext(DbContextOptions<MenuContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishIngredient>().HasKey(di => new
+            {
+                di.DishId,
+                di.IngredientId
+            });
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+            modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.DishIngredients).HasForeignKey(i => i.IngredientId);
+
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish
+                {
+                    Id = 1,
+                    Name = "Margherita",
+                    Price = 7.5,
+                    ImageUrl = "https://images.ctfassets.net/nw5k25xfqsik/64VwvKFqxMWQORE10Tn8pY/200c0538099dc4d1cf62fd07ce59c2af/20220211142754-margherita-9920.jpg?w=1024"
+                });
+            modelBuilder.Entity<Ingredient>().HasData
+                (
+                    new Ingredient 
+                    { 
+                        Id = 1, Name = "Tomato Sauce"
+                    }, 
+                    new Ingredient 
+                    { 
+                        Id = 2, Name = "Mozarella"
+                    }
+                );
+            modelBuilder.Entity<DishIngredient>().HasData
+                (
+                    new DishIngredient
+                    {
+                        DishId = 1, IngredientId = 1
+                    },
+
+                    new DishIngredient
+                    {
+                        DishId = 1, IngredientId = 2
+                    }
+                );
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<DishIngredient> DishIngredients { get; set; }
+
+    }
+}
