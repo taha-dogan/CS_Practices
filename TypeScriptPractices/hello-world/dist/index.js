6d329b6d9f367c9328d2270be94da373747de0a1
flakysob <flakysob@gmail.com>
2024-05-11 17:03:05 +0300

    TypeScript Practice with "Programming with Mosh" [edited]

--- TypeScriptPractices/hello-world/dist/index.js
+++ TypeScriptPractices/hello-world/dist/index.js
@@ -1,5 +1,8 @@
 "use strict";
-;
-let mySize = 2;
-console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000, 2023);
 //# sourceMappingURL=index.js.map
