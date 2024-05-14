ffa2d876cb534360e7775ab4a514c403282d73cf
Taha <71967160+taha-dogan@users.noreply.github.com>
2024-05-14 10:25:08 +0300

    Add files via upload
    Started A to Z ASP.NET Core Practice

---
+++ AToZAspNetCore/OrnekUygulama/OrnekUygulama/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekUygulama.Models;
+
+namespace OrnekUygulama.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        #region ViewResult
+
+        //public ViewResult Index()
+        //{
+        //    return View();
+        //}
+
+        #endregion
+
+        #region PartialViewResult  (Sadece belirli bir alanı Ajax teknolojisi ile Client tabanlı render eder.)
+
+        //public PartialViewResult Index()
+        //{
+        //    PartialViewResult result = PartialView();
+        //    return result;
+        //}
+
+        #endregion
+
+        #region JsonResult
+
+        //public JsonResult Index()
+        //{
+        //    JsonResult result = Json(new Product
+        //    {
+        //        Id = 1,
+        //        Name = "Deneme",
+        //        Quantity = 10,
+        //    });
+
+        //    return Json(result);
+        //}
+
+        #endregion
+
+
+    }
+}
