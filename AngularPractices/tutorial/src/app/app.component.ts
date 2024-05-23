3d3b6866d08b6e57b0fa5f510aad3b36d88c8a06
taha-dogan <flakysob@gmail.com>
2024-05-23 13:26:00 +0300

    Importing and creating project

---
+++ AngularPractices/tutorial/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HomeComponent]
+})
+export class AppComponent {
+  title = 'tutorial';
+}
+
+// Uygulamanın kök bileşenidir. İçerik, işlevsellik ve bileşen yapısı burada tanımlanır.
